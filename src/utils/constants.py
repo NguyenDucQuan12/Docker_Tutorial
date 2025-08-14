90c3bcabdf5f78d3f61ee5e63d8ff73ace21ad3e
unknown <nguyenducquan2001@gmail.com>
2025-08-14 15:44:46 +0700

    Cập nhật các hằng số

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,2 +1,8 @@
 DEFAULT_AVATAR = "images/default/avatar.png"
-DEFAULT_PRIVILEGE = "normal_user"
+
+# Danh sách các quyền hạn
+DEFAULT_PRIVILEGE = "Guest"
+BOSS_PRIVILEGE = "Boss"
+HIGH_PRIVILEGE_LIST = ["Admin", "Boss"]
+PRIVILEGE_LIST = ["Guest", "User", "Admin"]
+FULL_PRIVILEGE_LIST = ["Guest", "User", "Admin", "Boss"]
