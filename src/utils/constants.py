3513b80247f809af0012535ba69b7a68a65fd15f
unknown <nguyenducquan2001@gmail.com>
2025-08-20 10:22:23 +0700

    Xóa địa chỉ IP và PORT của máy chủ

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,7 +6,3 @@ BOSS_PRIVILEGE = "Boss"
 HIGH_PRIVILEGE_LIST = ["Admin", "Boss"]
 PRIVILEGE_LIST = ["Guest", "User", "Admin"]
 FULL_PRIVILEGE_LIST = ["Guest", "User", "Admin", "Boss"]
-
-# Địa chỉ host
-IP_ADDRESS_HOST = "10.239.2.63"
-PORT_HOST = 8000
